615ff20c51bd14ccd1a40f3059e49d79d4fb35fc
FullViper2406 <119512140+FullViper2406@users.noreply.github.com>
2023-04-26 11:24:58 -0300

    botao adicionado

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BotaoLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BotaoLogin
+            // 
+            this.BotaoLogin.Location = new System.Drawing.Point(366, 297);
+            this.BotaoLogin.Name = "BotaoLogin";
+            this.BotaoLogin.Size = new System.Drawing.Size(75, 23);
+            this.BotaoLogin.TabIndex = 0;
+            this.BotaoLogin.Text = "Login";
+            this.BotaoLogin.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BotaoLogin);
+            this.Name = "Login";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BotaoLogin;
     }
 }
 
