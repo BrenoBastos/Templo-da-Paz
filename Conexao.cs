305c7a9a93138baed34b4298957a67a0cd0cbdd2
FullViper2406 <119512140+FullViper2406@users.noreply.github.com>
2023-04-26 16:39:55 -0300

    inicio do BancoDeDados

---
+++ Conexao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace WindowsFormsApp1
+{
+    public  class Conexao
+    {
+        public static MySqlConnection con = new MySqlConnection("server=127.0.0.1:3307;database=necroterio;user id=root;password=root;");
+        public void Abrir(){
+            con.Open();
+        }
+        public void Fechar()
+        {
+            con.Close();
+        }
+    }
+}
